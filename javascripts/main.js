8a6871d4a590578122c137fd7130cd1292257c61
michael <michael@buybox-me.com>
2015-09-29 15:45:50 +0200

    initial commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,45 +1,52 @@
-const InfiniteScroll = require('./../js/infiniteScroll.js');
-// FAKER
-function slicer(data){
-    var i,j,temparray,chunk = 20;
-    for (i=0,j=data.length; i<j; i+=chunk) {
-        temparray = data.slice(i,i+chunk);
+'use strict';
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _jsInfiniteScrollJs = require('./../js/infiniteScroll.js');
+
+var _jsInfiniteScrollJs2 = _interopRequireDefault(_jsInfiniteScrollJs);
+
+// FAKER
+function slicer(data) {
+    var i,
+        j,
+        temparray,
+        chunk = 20;
+    for (i = 0, j = data.length; i < j; i += chunk) {
+        temparray = data.slice(i, i + chunk);
     }
     return temparray;
 }
-InfiniteScroll.prototype.faker = function() {
-        var getJSON = function (url) {
-            var xhr = typeof XMLHttpRequest != 'undefined'
-                ? new XMLHttpRequest()
-                : new ActiveXObject('Microsoft.XMLHTTP');
-            xhr.open('get', url, true);
-            xhr.onreadystatechange = function() {
-                var status;
-                var data;
-                // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
-                if (xhr.readyState == 4) { // `DONE`
-                    status = xhr.status;
-                    if (status == 200) {
-                        data = JSON.parse(xhr.responseText);
-                        data = slicer(data);
-                        this.handleResponse(data);
-                    } else {
-
-                    }
-                }
-            }.bind(this);
-            xhr.send();
-        };
+_jsInfiniteScrollJs2['default'].prototype.faker = function () {
+    var getJSON = function getJSON(url) {
+        var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        xhr.open('get', url, true);
+        xhr.onreadystatechange = (function () {
+            var status;
+            var data;
+            // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
+            if (xhr.readyState == 4) {
+                // `DONE`
+                status = xhr.status;
+                if (status == 200) {
+                    data = JSON.parse(xhr.responseText);
+                    data = slicer(data);
+                    this.handleResponse(data);
+                } else {}
+            }
+        }).bind(this);
+        xhr.send();
     };
+};
 
-    new InfiniteScroll('exampleTable', {
-        transport: 'faker',
-        url: '/example/exampleData.json',
-        fields: {
-            name:{},
-            balance:{},
-            phone:{}
-        }
-    })
+new _jsInfiniteScrollJs2['default']('exampleTable', {
+    transport: 'faker',
+    url: '/example/exampleData.json',
+    fields: {
+        name: {},
+        balance: {},
+        phone: {}
+    }
+});
 
+//# sourceMappingURL=main.js.map
